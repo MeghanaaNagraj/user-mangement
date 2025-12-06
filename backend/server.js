49483e4f0b3bf7bb93ab8dcc6849ae1930221d92
MeghanaaNagraj <meghanaa2109@gmail.com>
2025-12-06 18:10:14 +0530

    Update server.js cors

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,9 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  methods: ["GET","POST","PUT","DELETE"],
+  allowedHeaders: ["Content-Type","Authorization"]
 }));
-
-app.options("*", cors());
 
 mongoose
   .connect(process.env.MONGO_URI)
